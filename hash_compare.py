5320a3700c6ed2638b0a39a15542edf55d5d909d
Eduardo Batista <58524485+eduhbg@users.noreply.github.com>
2021-09-18 10:07:08 -0300

    upload file

---
+++ hash_compare.py
@@ -0,0 +1,22 @@
+import hashlib
+
+def hash_compare(hashed_password, hash_to_compare):
+    if hashed_password == hash_to_compare: return True
+    else: return False
+
+def convert_to_hash(password):
+    password = hashlib.md5(password.encode('utf8')).hexdigest()
+    return password
+
+def main():
+    password = input('Type your password: ')
+    hashed_password = convert_to_hash(password)
+    print('The md5 hash of your password is:', hashed_password)
+    hash_to_compare = input('Type a hash to compare: ')
+    result = hash_compare(hashed_password, hash_to_compare)
+
+    if result: print('Passwords match.')
+    else: print('The passwords do not match.')
+
+if __name__ == "__main__":
+    main()
